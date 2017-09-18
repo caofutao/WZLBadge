3dc7ea99c37fb035cc4bf235695d2c8a7c581386
caofutao <futaocao@126.com>
2017-09-18 23:30:28 +0800

    test1

--- WZLBadgeDemo/AppDelegate.m
+++ WZLBadgeDemo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //cft git test111
     return YES;
 }
 
